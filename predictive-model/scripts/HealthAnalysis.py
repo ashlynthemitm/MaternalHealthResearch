35997621fffaf6662385461c0bc4aabc6a8ac4d0
Ashlyn Campbell <ashlyncmitm@gmail.com>
2024-04-02 22:00:36 -0400

    Completed modelling of anomalies analysis

--- predictive-model/scripts/HealthAnalysis.py
+++ predictive-model/scripts/HealthAnalysis.py
@@ -1,6 +1,7 @@
 '''
 @author: Ashlyn Campbell
 File Description: This file takes in a csv file at the moment and will use the Fitbit API
+File Process: Daily Analysis and Overall Analysis Stats 
 '''
 
 from dotenv import load_dotenv
@@ -12,15 +13,25 @@ import os
 load_dotenv()
 os.chdir(os.getenv('DEFAULT_PATH'))
 
-class HealthAnalysis:
+class HealthAnalysis: # Risk Assessment
     def __init__(self, id):
-        self.id = id
+        self.id = id # may use name instead of id for future usage
         self.analysis = {
             'irregular_heartrate': False,
             'irregular_sleep': False,
             'irregular_activity': False,
+            'irregular_blood_pressure': False,
+            'irregular_symptoms': []
         }
-class HeartRateCorrelation:
+    
+    def store_irregularities(self):
+        # different tables for regular analysis versus irregular analysis
+        codehere=None
+        
+    def Alert(self):
+        print (f'Hello {self.id}, we have noticed something. Click the App for additional details!')
+        
+class HeartRateCorrelation(HealthAnalysis):
     def __init__(self):
         self.base_il = 0 # base intensity level
         self.base_mets = 10 # base mets value
@@ -34,8 +45,12 @@ class HeartRateCorrelation:
     def analyze_hr(self):
         return True # healthy possibility=True unhealthy=False
 
-    
-class SymptomCorrelation:
+    def Alert(self):
+        super().Alert() # initial message
+        print('Hello! Your heart rate has shown variability. Have you started any new habits?') # In the App Alert Message
+        
+
+class SymptomCorrelation(HealthAnalysis):
     # Manual Input use against heartrate
     def __init__(self): # typical conditions of pregnant women 
         self.typical_symptoms = {
@@ -55,48 +70,57 @@ class SymptomCorrelation:
         
     def analyze_bp(self):
         return True
+    
     def analyze_bmi(self):
         return 30
+    
+    def Alert(self):
+        super().Alert() # initial message
+        print('Hello! Your symptoms seem to be consistent with: ') # In the App Alert Message # high blood pressure, ov
+        
 
-class SleepCorrelation():
-    print(None) # sleep is a direct link: https://pubmed.ncbi.nlm.nih.gov/29103944/
+class SleepCorrelation(HealthAnalysis): # sleep is a direct link: https://pubmed.ncbi.nlm.nih.gov/29103944/
     def __init__(self, total_minutes):
         self.total_minutes = total_minutes
+        self.irregular_sleep, self.type_sleep = self.analyzeSleep()
         
     def analyzeSleep(self):
         total_hours = self.total_minutes // 60 # convert minutes into hours analysis
         irregular_sleep = False
+        type_sleep = 'normal'
         if (total_hours <= 6):
             irregular_sleep = True
-        self.Alert() # alert with sleep metric
-        return irregular_sleep # irregular sleep can be caused by many things so the next steps is to perform a Health Analysis 
+            type_sleep = 'little'
+            self.Alert() # alert with sleep metric
+        elif (total_hours >= 10):
+            irregular_sleep = True
+            type_sleep = 'excessive'
+            self.Alert() # alert with sleep metric
+        return irregular_sleep, type_sleep # irregular sleep can be caused by many things so the next steps is to perform a Health Analysis 
         
     def Alert(self):
         # customize types of messages based on the scenario # update this later
         SummarizeData.displayAdvice(type='improve_sleep') # the future may yield exact requests input
-        return f'''
-                Hello first_name, we have noticed a decline in sleep time. Click the App to learn how to improve these patterns!
-                '''
+        print(f'We have noticed a {self.type_sleep} in sleep time. Click the App to learn how to improve these patterns!')
     
-class ActivityCorrelation(): # read articles on how activity can benefit mental health, obesity, vitals etc.
+class ActivityCorrelation(HealthAnalysis): # read articles on how activity can benefit mental health, obesity, vitals etc.
     print(None) 
     
-class MentalHealthCorrelation():
+class MentalHealthCorrelation(HealthAnalysis):
     def __init__(self):
         self.mental_health_code = {
             'mood_swings': ['bipolar'] # work on the correlations between various mental illnesses and maternal health
         }
-        
-class WeightCorrelation(): # analysis of diabetes and obesity: https://pubmed.ncbi.nlm.nih.gov/20963519/
+   
+class WeightCorrelation(HealthAnalysis): # analysis of diabetes and obesity: https://pubmed.ncbi.nlm.nih.gov/20963519/
     print(None)
 
 ## Geospatial component
-class SubstanceAbuseCorrelation(): # maybe tie in symptoms, mental health, sleep patterns etc. to potential substance abuse
+class SubstanceAbuseCorrelation(HealthAnalysis): # maybe tie in symptoms, mental health, sleep patterns etc. to potential substance abuse
     print(None)
     
 
-class DeathGeoRateCorrelation(): # death rate correlation to geography areas using WONDER data, data of hospitals, and public/private insurance, and black population (women)
+class DeathGeoRateCorrelation(HealthAnalysis): # death rate correlation to geography areas using WONDER data, data of hospitals, and public/private insurance, and black population (women)
     print(None)
     
         
-    
